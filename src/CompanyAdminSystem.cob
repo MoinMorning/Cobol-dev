7618ec9558e0f18b1d84d1a8126ac1b1bee58818
Minh Le <moin.morning2000@gmail.com>
2024-05-07 11:50:36 +0200

    change companyadmin code -> Begin MenuOption

---
+++ src/CompanyAdminSystem.cob
@@ -0,0 +1,50 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. CompanyAdminSystem.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 MenuOption PIC 9.
+01 MachineNumber PIC 9(5) VALUE 0.
+01 MachineList.
+   05 MachineNum PIC 9(5) OCCURS 100 TIMES.
+
+PROCEDURE DIVISION.
+Begin.
+    MOVE 0 TO MenuOption
+    PERFORM UNTIL MenuOption = 3
+        PERFORM MenuOption
+    END-PERFORM.
+    STOP RUN.
+
+MenuOption.
+    DISPLAY "Company Admin System"
+    DISPLAY "1. Add Machine Number"
+    DISPLAY "2. Check Machine Numbers"
+    DISPLAY "3. Exit"
+    ACCEPT MenuOption
+    PERFORM MenuAction.
+
+MenuAction.
+    EVALUATE MenuOption
+        WHEN 1
+            PERFORM AddMachine
+        WHEN 2
+            PERFORM CheckMachineNumbers
+        WHEN 3
+            EXIT PROGRAM
+        WHEN OTHER
+            DISPLAY "Invalid option. Please try again."
+    END-EVALUATE.
+
+AddMachine.
+    DISPLAY "Enter Machine Number to add: "
+    ACCEPT MachineNumber
+    ADD MachineNumber TO MachineList
+    PERFORM MenuOption.
+
+CheckMachineNumbers.
+    DISPLAY "Machine Numbers in the company:"
+    PERFORM VARYING MachineIndex FROM 1 BY 1 UNTIL MachineIndex > 100
+        DISPLAY MachineList(MachineIndex)
+    END-PERFORM.
+    PERFORM MenuOption.
