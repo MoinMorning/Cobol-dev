f435f9185635ab395b139d90f5e401e36a4b0822
Moin Moin <79722687+MoinMorning@users.noreply.github.com>
2024-05-09 18:25:20 +0200

    Update CompanyAdminSystem.cbl
    Fix column issues - logic have to be updated

--- src/CompanyAdminSystem.cbl
+++ src/CompanyAdminSystem.cbl
@@ -1,29 +1,30 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. CompanyAdminSystem.
-AUTHOR. lek.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CompanyAdminSystem.
+       AUTHOR. lek.
 
-DATA DIVISION.
-WORKING-STORAGE SECTION.
-01 MenuOption PIC 9.
-01 MachineNumber PIC 9(5) VALUE 0.
-01 MachineList.
-   05 MachineNum PIC 9(5) OCCURS 100 TIMES.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 ws-MenuOption PIC 9.
+       01 ws-MachineNumber PIC 9(5) VALUE 0.
+       01 ws-MachineIndex PIC 9(3) VALUE 1.
+       01 ws-MachineList.
+       05 ws-MachineNum PIC 9(5) OCCURS 100 TIMES.
 
-PROCEDURE DIVISION.
-Begin.
-    PERFORM MenuOption UNTIL MenuOption = 3
-    STOP RUN.
+       PROCEDURE DIVISION.
+       Begin.
+       PERFORM DisplayMenu UNTIL ws-MenuOption = 3
+       STOP RUN.
 
-MenuOption.
-    DISPLAY "Company Admin System"
-    DISPLAY "1. Add Machine Number"
-    DISPLAY "2. Check Machine Numbers"
-    DISPLAY "3. Exit"
-    ACCEPT MenuOption
-    PERFORM MenuAction.
+       DisplayMenu.
+       DISPLAY "Company Admin System"
+       DISPLAY "1. Add Machine Number"
+       DISPLAY "2. Check Machine Numbers"
+       DISPLAY "3. Exit"
+       ACCEPT ws-MenuOption
+       PERFORM MenuAction.
 
-MenuAction.
-    EVALUATE MenuOption
+       MenuAction.
+       EVALUATE ws-MenuOption
         WHEN 1
             PERFORM AddMachine
         WHEN 2
@@ -32,21 +33,20 @@ MenuAction.
             EXIT PROGRAM
         WHEN OTHER
             DISPLAY "Invalid option. Please try again."
-    END-EVALUATE.
+       END-EVALUATE.
 
-AddMachine.
-    DISPLAY "Enter Machine Number to add: "
-    ACCEPT MachineNumber
-    ADD 1 TO MachineList(MachineNumber)
-    PERFORM MenuOption.
+       AddMachine.
+       DISPLAY "Enter Machine Number to add: "
+       ACCEPT ws-MachineNumber
+       MOVE ws-MachineNumber TO ws-MachineNum(ws-MachineIndex)
+       ADD 1 TO ws-MachineIndex
+       PERFORM DisplayMenu.
 
-CheckMachineNumbers.
-    DISPLAY "Machine Numbers in the company:"
-    PERFORM VARYING MachineIndex FROM 1 BY 1 UNTIL MachineIndex > 100
-        IF MachineList(MachineIndex) NOT = 0
-            DISPLAY MachineList(MachineIndex)
-    END-PERFORM.
-    PERFORM MenuOption.
-
-BEGIN.
-    PERFORM Begin.
+       CheckMachineNumbers.
+       DISPLAY "Machine Numbers in the company:"
+           PERFORM VARYING ws-MachineIndex FROM 1 BY 1 UNTIL 
+           ws-MachineIndex > 100
+           IF ws-MachineNum(ws-MachineIndex) NOT = 0
+               DISPLAY ws-MachineNum(ws-MachineIndex)
+           END-PERFORM
+       PERFORM DisplayMenu.
