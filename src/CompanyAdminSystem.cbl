d5a538b765eb07e5adbc1513f0697bc3473ecb20
Moin Moin <79722687+MoinMorning@users.noreply.github.com>
2024-05-19 17:18:52 +0200

    Now the name of user could bge updated!!

--- src/CompanyAdminSystem.cbl
+++ src/CompanyAdminSystem.cbl
@@ -5,36 +5,53 @@
        ENVIRONMENT DIVISION. 
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT MachineFile ASSIGN TO "machines.txt"
-                  ORGANIZATION IS LINE SEQUENTIAL
-                  ACCESS MODE IS SEQUENTIAL.
+           SELECT MachineFile
+           ASSIGN TO "machines.txt"
+           ORGANIZATION IS LINE SEQUENTIAL
+           ACCESS MODE IS SEQUENTIAL
+           FILE STATUS IS ws-MachineFile-Status.
 
-           SELECT TempMachineFile ASSIGN TO "temp_machines.txt"
-                  ORGANIZATION IS LINE SEQUENTIAL
-                  ACCESS MODE IS SEQUENTIAL.
+           SELECT TempMachineFile
+           ASSIGN TO 
+           "temp_machines.txt"
+           ORGANIZATION IS LINE SEQUENTIAL
+           ACCESS MODE IS SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
        FD MachineFile.
        01 MachineRecord.
-           05 MachineNum PIC 9(5).
+           05 MachineNum PIC X(5).
            05 MachineTyp PIC X(5).
            05 MachineManu PIC X(5).
+           05 MachineUsername PIC X(20).
+           05 MachineSpec PIC X(50).
 
        FD TempMachineFile.
        01 TempMachineRecord.
-           05 TempMachineNum PIC 9(5).
+           05 TempMachineNum PIC X(5).
            05 TempMachineTyp PIC X(5).
            05 TempMachineManu PIC X(5).
 
        WORKING-STORAGE SECTION.
+       01 ws-MachineFile-Status PIC 99. 
        01 ws-MenuOption PIC 9.
        01 ws-MachineNumber PIC 9(5) VALUE 0.
        01 ws-MachineIndex PIC 9(3) VALUE 1.
        01 ws-MachineList.
            05 ws-MachineNum PIC 9(5) OCCURS 100 TIMES.
+       01 Username PIC X(20).
+       01 ws-MachineSpec PIC X(50).
 
        PROCEDURE DIVISION.
+           
+           OPEN INPUT MachineFile
+           IF ws-MachineFile-Status NOT EQUAL 00
+              DISPLAY "Error opening MachineFile: " 
+              ws-MachineFile-Status
+             STOP RUN.
+           
+       
        Begin.
            PERFORM DisplayMenu UNTIL ws-MenuOption = 5
            STOP RUN.
@@ -64,8 +81,32 @@
        END-EVALUATE.
 
        AddMachine.
+           
            DISPLAY "Enter Machine Number to add: "
-       ACCEPT ws-MachineNumber
+           ACCEPT ws-MachineNumber
+           IF ws-MachineNumber LESS THAN 1 OR ws-MachineNumber GREATER 
+           THAN 99999
+           DISPLAY "Invalid Machine Number. Please enter a number betwe"
+           "en 1 and 99999."
+           GO TO DisplayMenu 
+           END-IF.
+
+           DISPLAY "Enter Username: "
+           ACCEPT Username
+           IF Username EQUAL SPACES
+           DISPLAY "Username cannot be empty."
+           GO TO DisplayMenu
+           END-IF. 
+
+
+           DISPLAY "Enter Machine Specifications: "
+           ACCEPT ws-MachineSpec 
+
+           IF ws-MachineSpec EQUAL SPACES
+           DISPLAY "Machine Specifications cannot be empty."
+           GO TO DisplayMenu
+           END-IF.
+
            PERFORM AppendMachineToFile
            ADD 1 TO ws-MachineIndex
            PERFORM DisplayMenu.
@@ -84,6 +125,7 @@
             END-PERFORM
            CLOSE MachineFile
            PERFORM DisplayMenu.
+
        DeleteMachine.
            DISPLAY "Enter Machine Number to delete: "
            ACCEPT ws-MachineNumber 
@@ -126,11 +168,21 @@
                         DISPLAY "Machine Number not found."
                         EXIT PERFORM 
             END-PERFORM
-              CLOSE MachineFile
-               PERFORM DisplayMenu.
+            CLOSE MachineFile
+            PERFORM DisplayMenu.
        
        AppendMachineToFile.
            OPEN EXTEND MachineFile
-           WRITE MachineRecord FROM ws-MachineNumber
+           MOVE ws-MachineNumber TO MachineNum
+           MOVE Username TO MachineUsername  OF MachineRecord
+           MOVE MachineSpec  TO MachineSpec OF MachineRecord
+           WRITE MachineRecord
            CLOSE MachineFile.
+
+
   
+           COMPUTE ws-MachineIndex = ws-MachineIndex + 1 ON SIZE ERROR
+           DISPLAY "Error: Overflow occurred when incrementing"
+           "ws-MachineIndex."
+           STOP RUN
+           END-COMPUTE.
